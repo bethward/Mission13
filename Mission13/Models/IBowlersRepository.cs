137c6544228ec0e7f4a95d8482dda8e0ae76c268
bethward <88352499+bethward@users.noreply.github.com>
2022-03-30 23:46:37 -0600

    first

---
+++ Mission13/Models/IBowlersRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Mission13.Models
+{
+    public interface IBowlersRepository
+    {
+        IQueryable<Bowler> Bowlers { get; }
+
+        public void SaveChanges(Bowler b);
+
+        public void AddBowler(Bowler b);
+
+        public void DeleteBowler(Bowler b);
+
+    }
+}
